3e454047c8d3f6a2bf101d53dff2800ad990f260
Mauroedugus <maurogus005@gmail.com>
2025-03-15 16:36:40 -0300

    init project

---
+++ librumbr/src/main/java/br/com/librumbr/LibrumbrApplication.java
@@ -0,0 +1,13 @@
+package br.com.librumbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrumbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrumbrApplication.class, args);
+	}
+
+}
---
+++ librumbr/src/test/java/br/com/librumbr/LibrumbrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.librumbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrumbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
